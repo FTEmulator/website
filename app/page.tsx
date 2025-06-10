ac063ceb07287e78ddca5bd2eb27e556319a8b5d
alexwebdev05 <alexwebdev05@proton.me>
2025-06-10 16:18:36 +0200

    test12

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Test12
+            Test13
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
